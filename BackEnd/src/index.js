6d82ff8741c62f00f3d889605a9e8167609a3280
Leonardo <leo.luz14@hotmail.com>
2019-09-15 23:00:53 -0300

    criando parte logica

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,3 +1,5 @@
 const express = require('express') ;
 
 const app = express(); // express cria um servidor para ser acessado pela minha aplicação
+
+app.listen(3030);
--- ValeEntrosa/src/pages/Home.js
+++ ValeEntrosa/src/pages/Home.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+
 
 export default function Home() {
+
+  const botaoMais = () => {
+    cePoint = cePoint + 1
+    return cePoint;
+  }
+  const botaoMenos = () => {
+    cePoint = cePoint - 1
+    return cePoint;
+  }
+  var cePoint = 50;
   return (
+    
     <View style={styles.container}>
       <Text style={styles.textOne}>Você possui:</Text>
       <View style={styles.pointCard}>
         <View>
-          <Text style={styles.textTwo}>50</Text>
+          <Text style={styles.textTwo}>{cePoint}</Text>
         </View>
         <View style={styles.ma}>
           <Text style={styles.textThree}>ce</Text>
         </View>
+      </View>
+      <View style={styles.containerButton}>
+        <TouchableOpacity onPress={botaoMenos} style={styles.button}>
+            <Text style={styles.buttonMenos}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={botaoMais} style={styles.button}>
+            <Text style={styles.buttonMais}>+</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -49,4 +70,31 @@ const styles = StyleSheet.create({
   ma: {
     marginTop: 180,
   },
+  button:{
+    borderRadius: 80,
+    height: 70,
+    width: 70,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    marginHorizontal:10
+  },
+  buttonMais:{
+    fontSize:48,
+    color: '#f5f5f5',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    elevation: 10,
+    shadowColor: '#f5f5f5'
+  },
+  buttonMenos:{
+    fontSize:48,
+    color: '#f5f5f5',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    elevation: 10,
+    shadowColor: '#f5f5f5'
+  },
+  containerButton:{
+    flexDirection: 'row'
+  }
 });
